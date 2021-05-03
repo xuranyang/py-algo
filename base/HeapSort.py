ef4d81d131baa46bc4612a7ade95118c8ab15f7a
xuranyang <18757365877@163.com>
2021-05-04 01:47:32 +0800

    堆排序

--- base/HeapSort.py
+++ base/HeapSort.py
@@ -169,7 +169,46 @@ def heap_sort(arr: list):
     return arr
 
 
+"""
+堆排序 写法二
+"""
+
+
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1  # left = 2*i + 1
+    r = 2 * i + 2  # right = 2*i + 2
+
+    if l < n and arr[i] < arr[l]:
+        largest = l
+
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  # 交换
+
+        heapify(arr, n, largest)
+
+
+def heapSort(arr):
+    n = len(arr)
+
+    # Build a maxheap.
+    for i in range(n, -1, -1):
+        heapify(arr, n, i)
+
+        # 一个个交换元素
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  # 交换
+        heapify(arr, i, 0)
+
+    return arr
+
+
 if __name__ == '__main__':
     li = [14, 2, 34, 43, 21, 19]
-    sorted_li = heap_sort(li)
+    # sorted_li = heap_sort(li)
+    # print(sorted_li)
+    sorted_li = heapSort(li)
     print(sorted_li)
