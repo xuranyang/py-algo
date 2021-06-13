c7c5ec1ccb4bbf26d2c234a7bfaef543e85dcd6e
xuranyang <18757365877@163.com>
2021-06-14 00:31:01 +0800

    求二叉树的层序遍历

--- nowcoder/求二叉树的层序遍历.py
+++ nowcoder/求二叉树的层序遍历.py
@@ -14,15 +14,17 @@ class Solution:
         # write code here
         if root is None:
             return []
-
+        # 初始化队列
         queue = [root]
-
+        # 初始化最终层序遍历 结果列表
         res = []
         while len(queue) > 0:
+            # 初始化每一层 val 的list
             level_res = []
-            for i in range(len(queue)):
+            for _ in range(len(queue)):
                 # 遍历取出当前层的每一个节点
                 cur = queue.pop(0)
+                # 取出当前层节点的val,放入该层的list
                 level_res.append(cur.val)
                 # 下一层的左节点
                 if cur.left is not None:
@@ -30,6 +32,31 @@ class Solution:
                 # 下一层的右节点
                 if cur.right is not None:
                     queue.append(cur.right)
+            # 放入最终结果的列表中
             res.append(level_res)
 
         return res
+
+
+"""
+   3
+9     20
+    15  7
+
+返回结果:
+[
+    [3],
+    [9,20],
+    [15,7]
+]
+"""
+if __name__ == '__main__':
+    tree = TreeNode(3)
+    tree.left = TreeNode(9)
+    tree_son = TreeNode(20)
+    tree_son.left = TreeNode(15)
+    tree_son.right = TreeNode(7)
+    tree.right = tree_son
+
+    level_order_res = Solution().levelOrder(tree)
+    print(level_order_res)
