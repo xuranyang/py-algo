361ae2dab3dea06224347ee890cefe2f84f9fb07
xry <362438997@qq.com>
2021-06-15 00:07:00 +0800

    寻找第K大

--- nowcoder/寻找第K大.py
+++ nowcoder/寻找第K大.py
@@ -3,6 +3,11 @@
 # 输入：[1,3,5,2,2],5,3
 # 返回值：2
 
+
+"""
+可以结合快排一起看
+在 前大后小 快排的基础上,增加了一个条件判断
+"""
 class Solution:
     def findKth(self, a, n, K):
         # write code here
@@ -12,13 +17,24 @@ class Solution:
         if left <= right:
             index = self.partition(arr, left, right)
 
+            # 因为index从0开始,所以k-1位置就是第k大的数
+            # 如果正好index就是第k大的数,该arr[index]就是结果
             if index == k - 1:
                 return arr[index]
+            # 此时,说明第k大的数比arr[index]更小
+            # 第k大的数,在arr中的位置一定是在 index+1及之后
             elif index < k - 1:
                 return self.quick_sort(arr, index + 1, right, k)
+            # 反之,index>=k-1,说明第k大的数 比 arr[index]更大
+            # 第k大的数,在arr中的位置一定是在 index-1及之前
             else:
                 return self.quick_sort(arr, left, index - 1, k)
 
+    # 根据 privot 前大后小 进行分区,
+    # 大于privot的前一个分区，
+    # 小于等于privot的后一个分区
+    # 第一次初始化时,left为0,返回值是 根据arr[0]进行分割的index
+    # 保证index之前都大于arr[0],index之后都小于等于arr[0]
     def partition(self, arr, left, right):
         privot = arr[left]
         while left < right:
