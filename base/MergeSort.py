76a84a4826545b8697807e3113e97c7140161b89
xuranyang <xu.ranyang@yalla.live>
2024-03-06 11:26:21 +0800

    MergeSort

--- base/MergeSort.py
+++ base/MergeSort.py
@@ -13,16 +13,16 @@
 """
 
 
-def merge_sort(lst):
-    lst_length = len(lst)
-    if lst_length <= 1:
-        return lst
+def merge_sort(arr):
+    arr_len = len(arr)
+    if arr_len <= 1:
+        return arr
 
     # 将列表分成两半
-    mid = lst_length // 2
+    mid = arr_len // 2
     # 递归地对左右两部分进行归并排序
-    left = merge_sort(lst[:mid])
-    right = merge_sort(lst[mid:])
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
 
     print(left)
     print(right)
@@ -31,30 +31,30 @@ def merge_sort(lst):
     return merge(left, right)
 
 
-def merge(left_lst, right_lst):
+def merge(left_arr, right_arr):
     result = []
-    left_index = right_index = 0
+    left_idx = right_idx = 0
 
-    while left_index < len(left_lst) and right_index < len(right_lst):
+    while left_idx < len(left_arr) and right_idx < len(right_arr):
         # 如果当前左边数组的元素 小于等于 当前右边数组的元素 则 放入最终结果列表 并将左边数组的下标+1
-        if left_lst[left_index] <= right_lst[right_index]:
-            result.append(left_lst[left_index])
-            left_index += 1
+        if left_arr[left_idx] <= right_arr[right_idx]:
+            result.append(left_arr[left_idx])
+            left_idx += 1
         # 如果当前右边数组的元素 小于 当前左边数组的元素 则 放入最终结果列表 并将右边数组的下标+1
         # 简单写可以直接else
         # else
-        elif right_lst[right_index] < left_lst[left_index]:
-            result.append(right_lst[right_index])
-            right_index += 1
+        elif right_arr[right_idx] < left_arr[left_idx]:
+            result.append(right_arr[right_idx])
+            right_idx += 1
     # 说明左边数组的数字已经全部取完,将右边数组剩余的数字全部放进最终结果即可
-    if left_index == len(left_lst):
-        for i in right_lst[right_index:]:
+    if left_idx == len(left_arr):
+        for i in right_arr[right_idx:]:
             result.append(i)
     # 否则说明右边数组的数字已经全部取完,将左边数组剩余的数字全部放进最终结果即可
     # 简单写可以直接else
     # else
-    elif right_index == len(right_lst):
-        for j in left_lst[left_index:]:
+    elif right_idx == len(right_arr):
+        for j in left_arr[left_idx:]:
             result.append(j)
 
     return result
@@ -62,5 +62,6 @@ def merge(left_lst, right_lst):
 
 if __name__ == '__main__':
     arr = [14, 2, 34, 43, 21, 19]
+    # arr = [3, 1, 7, 2, 5, 4, 9, 6, 8]
     sorted_arr = merge_sort(arr)
     print(sorted_arr)
