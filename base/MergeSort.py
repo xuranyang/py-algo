a9b49c927c6d57654e2ca39ec0d0850bdaab5ffc
xuranyang <xu.ranyang@yalla.live>
2024-03-06 11:20:23 +0800

    MergeSort

--- base/MergeSort.py
+++ base/MergeSort.py
@@ -18,14 +18,16 @@ def merge_sort(lst):
     if lst_length <= 1:
         return lst
 
+    # 将列表分成两半
     mid = lst_length // 2
+    # 递归地对左右两部分进行归并排序
     left = merge_sort(lst[:mid])
     right = merge_sort(lst[mid:])
 
     print(left)
     print(right)
     print("======")
-
+    # 合并排序后的左右两部分
     return merge(left, right)
 
 
@@ -38,8 +40,9 @@ def merge(left_lst, right_lst):
         if left_lst[left_index] <= right_lst[right_index]:
             result.append(left_lst[left_index])
             left_index += 1
-        # 简单写可以直接else
         # 如果当前右边数组的元素 小于 当前左边数组的元素 则 放入最终结果列表 并将右边数组的下标+1
+        # 简单写可以直接else
+        # else
         elif right_lst[right_index] < left_lst[left_index]:
             result.append(right_lst[right_index])
             right_index += 1
@@ -49,6 +52,7 @@ def merge(left_lst, right_lst):
             result.append(i)
     # 否则说明右边数组的数字已经全部取完,将左边数组剩余的数字全部放进最终结果即可
     # 简单写可以直接else
+    # else
     elif right_index == len(right_lst):
         for j in left_lst[left_index:]:
             result.append(j)
